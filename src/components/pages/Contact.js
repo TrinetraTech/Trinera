01f97f00764ba9b6e51f545fd202323535787bc1
eZice <144651926+esyice@users.noreply.github.com>
2025-05-26 00:41:19 +0530

    Update Contact.js
    youtube Icon remove ( unused var cause error in production  )

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -19,7 +19,6 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import YouTubeIcon from "@mui/icons-material/YouTube";
 import emailjs from "@emailjs/browser";
 
 const Contact = () => {
